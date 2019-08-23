abfb68bf10f0e5a3c6deb6287315c4586b8fc6c8
Diego Herculano <42254845+kwelf@users.noreply.github.com>
2019-08-23 17:00:41 -0300

    Update hello.py
    adicionado lista, contagem, e append

--- hello.py
+++ hello.py
@@ -12,5 +12,33 @@ else:
 print('A partir de agora você poderá cadastrar')
 nomecadastro = input('Digite um nome: ')
 idadecadastro = input('Digite uma idade: ')
+recordname = []
+recordidade = []
 
 print('Você cadastrou', nomecadastro, 'com idade de', idadecadastro, 'anos')
+recordname.append(nomecadastro)
+recordidade.append(idadecadastro)
+continuar = input('Deseja continuar? ')
+
+while continuar == str("sim") or continuar == str("s"):
+	nomecadastro = input('Digite um nome: ')
+	idadecadastro = input('Digite uma idade: ')
+	recordname.append(nomecadastro)
+	recordidade.append(idadecadastro)
+	print('Você cadastrou', nomecadastro, 'com idade de', idadecadastro, 'anos')
+	fim = input('Deseja continuar? ')
+	if fim == str("sim") or fim == str("s"):
+		print('Tudo bem.')
+	else:
+		break
+			
+print('Listando....')
+nomes = recordname
+idades = recordidade
+conta = len(nomes)
+print('Você digitou um total de', conta, 'cadastros', nome, 'obrigado.')
+for i in range(0, conta): 
+	print('O nome:', nomes[i], 'com a idade de:', idades[i], 'anos.')
+print('Fim... Até a próxima.')
+		
+
